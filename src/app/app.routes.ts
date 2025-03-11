40982de1d27305f70bb838c732e33d62a5bc6a27
willychen <willy77423@gmail.com>
2025-03-11 21:49:51 +0800

    增加子畫面login-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './direct-page/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginPageComponent }, // 設定根路徑的首頁
+    { path: 'login', component: LoginPageComponent }, // 設定 /login 路由，顯示 MainComponent
+];
---
+++ src/app/direct-page/direct-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direct-page',
+  imports: [],
+  templateUrl: './direct-page.component.html',
+  styleUrl: './direct-page.component.css'
+})
+export class DirectPageComponent {
+
+}

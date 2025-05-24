7f7f560b443095dc5beea3f41f8587c6030c8efd
willychen <willy77423@gmail.com>
2025-05-24 16:53:24 +0800

    node js23降版至18.17.1

---
+++ docs/browser/chunk-ZSDZLKAP.js
@@ -0,0 +1,1 @@
+import{Bb as a,Sa as o,jb as r,kb as n}from"./chunk-DA4IRPE4.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-error-page"]],decls:2,vars:0,template:function(e,m){e&1&&(r(0,"p"),a(1,"\u627E\u4E0D\u5230\u8DEF\u5F91"),n())},encapsulation:2})};export{p as ErrorPageComponent};
